8ec79bbed58a0e49d018c7d9c352756862499c03
Grzegorz G <greggupl1@gmail.com>
2020-01-11 20:38:28 +0100

    im trying ok

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,25 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+
+
 public class Controller {
+
+    @FXML
+            private Button exitButton;
+
+    public Controller() {
+        System.out.println("dziala");
+    }
+
+    @FXML
+    void initialize() {
+        System.out.println("init");
+        if (exitButton.isPressed()) System.exit(0);
+    }
+
+
+
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,15 +4,24 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(this.getClass().getResource("/sample.fxml"));
+
+        StackPane stackPane = loader.load();
+
+       // Controller controller = loader.getController();
+        System.out.println("main");
+       // Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        //Controller controller =
+        primaryStage.setTitle("GG Weather");
+        primaryStage.setScene(new Scene(stackPane));
         primaryStage.show();
     }
 
